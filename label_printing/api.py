14a8467d33e7f47cc013445daa764f24678f4978
CAnetzberger <frappe@erp1.ad.prothmann.com>
2022-01-31 15:24:52 +0100

    Printing done

---
+++ label_printing/api.py
@@ -0,0 +1,73 @@
+from __future__ import unicode_literals
+
+import frappe, os
+import json
+from frappe import _
+
+
+from frappe.utils.pdf import get_pdf,cleanup
+from frappe.core.doctype.access_log.access_log import make_access_log
+from PyPDF2 import PdfFileWriter
+
+
+@frappe.whitelist()
+def print_label(values):
+    values = json.loads(values)
+    printer_setting = frappe.db.get_single_value('Label Printer Settings', 'label_printer'),
+
+
+    for label in values["labels"]:
+        doc = frappe.new_doc('Label')
+        doc.item_code = values["item_code"]
+        doc.item_name = values["item_name"]
+        doc.delivery_date = values["delivery_date"]
+        doc.customer = values["customer"]
+        doc.batch = values["batch"]
+        doc.qty = label["item_qty"]
+        newdoc = doc.insert()
+
+        print_label_by_server("Label", newdoc.name, label["label_qty"], printer_setting[0],"Label", doc=None, no_letterhead=0, file_path=None)
+
+    return 200
+    
+
+def print_label_by_server(doctype, name, qty, printer_setting, print_format=None, doc=None, no_letterhead=0, file_path=None):
+    pdf_options = {
+        'page-height': frappe.db.get_single_value('Label Printer Settings', 'label_height'),
+        'page-width': frappe.db.get_single_value('Label Printer Settings', 'label_width'),
+    }
+    print_settings = frappe.get_doc("Network Printer Settings", printer_setting)
+    try:
+        import cups
+    except ImportError:
+        frappe.throw(_("You need to install pycups to use this feature!"))
+
+    try:
+        cups.setServer(print_settings.server_ip)
+        cups.setPort(print_settings.port)
+        conn = cups.Connection()
+        output = PdfFileWriter()
+        output = frappe.get_print(doctype, name, print_format, doc=doc, no_letterhead=no_letterhead, as_pdf = True, output = output, pdf_options=pdf_options)
+        print("Here")
+        print(output)
+        if not file_path:
+            file_path = os.path.join("/", "tmp", "frappe-pdf-{0}.pdf".format(frappe.generate_hash()))
+        output.write(open(file_path,"wb"))
+        for _ in range(qty):
+            conn.printFile(print_settings.printer_name,file_path , name, {})
+    except IOError as e:
+        if ("ContentNotFoundError" in e.message
+            or "ContentOperationNotPermittedError" in e.message
+            or "UnknownContentError" in e.message
+            or "RemoteHostClosedError" in e.message):
+            frappe.throw(_("PDF generation failed"))
+    except cups.IPPError:
+        frappe.throw(_("Printing failed"))
+    finally:
+        return
+
+
+@frappe.whitelist()
+def get_associated_stockentry(workorder):
+    return frappe.get_last_doc('Stock Entry', filters = {"work_order": "PRO-01530"})
+     
--- label_printing/public/js/label_printing_desk.js
+++ label_printing/public/js/label_printing_desk.js
@@ -1,8 +1,6 @@
-import LabelPrintingDialog from "./label_printing_dialog.vue";
-
 $(document).ready(function () {
-    $(".dropdown-notifications").after('<li id="labels-toolbar" class="nav-item"><a class="nav-link label-printing-icon text-muted"><i class="fa fa-tag fa-lg" aria-hidden="true"></i></a></li>');
-    $("#labels-toolbar").click(function(page) {
+  $(".dropdown-notifications").after('<li id="labels-toolbar" class="nav-item"><a class="nav-link label-printing-icon text-muted"><i class="fa fa-tag fa-lg" aria-hidden="true"></i></a></li>');
+  $("#labels-toolbar").click(function (page) {
     setupLabelsDialog(page);
   });
 
@@ -15,7 +13,7 @@ function setupLabelsDialog(page) {
 
   let fields = {
     doctype: '',
-    docname: ' ',
+    docname: '',
     item_code: '',
     item_name: '',
     delivery_date: '',
@@ -24,11 +22,9 @@ function setupLabelsDialog(page) {
     information: '',
     total_amount: 0,
     labels: [{
-      qty_per_label: 0,
-      is_warehouse: 0,
-      information: '',
-    }],
-    preview: __('Click "Render Preview"')
+      item_qty: 0,
+      label_qty: 0,
+    }]
   };
 
 
@@ -44,99 +40,100 @@ function setupLabelsDialog(page) {
   let d = new frappe.ui.Dialog({
     title: __("Print Labels"),
     fields: [{
-        label: __("Reference Doctype"),
-        options: ['Work Order', 'Item'],
-        fieldname: 'doctype',
-        fieldtype: 'Select',
-        default: fields.doctype,
+      label: __("Reference Doctype"),
+      options: ['Work Order', 'Item'],
+      fieldname: 'doctype',
+      fieldtype: 'Select',
+      default: fields.doctype,
+    },
+    {
+      label: __("Get data"),
+      fieldname: 'get_data',
+      fieldtype: 'Button',
+      click: () => {
+        handleDataFetch()
+      }
+    },
+    {
+      fieldtype: 'Column Break'
+    },
+    {
+      label: __("Reference Docname"),
+      fieldname: 'docname',
+      fieldtype: 'Data',
+      options: 'doctype',
+      default: fields.docname,
+    },
+    {
+      fieldtype: 'Section Break',
+      label: __('Information')
+    },
+    {
+      label: __("Item Code"),
+      fieldname: 'item_code',
+      fieldtype: 'Data'
+    },
+    {
+      label: __("Item Name"),
+      fieldname: 'item_name',
+      fieldtype: 'Data'
+    },
+    {
+      label: __("Delivery Date"),
+      fieldname: 'delivery_date',
+      fieldtype: 'Date'
+    },
+    {
+      fieldtype: 'Column Break'
+    },
+    {
+      label: __("Customer"),
+      fieldname: 'customer',
+      fieldtype: 'Data'
+    },
+    {
+      label: __("Batch"),
+      fieldname: 'batch',
+      fieldtype: 'Data'
+    },
+    {
+      fieldtype: 'Section Break',
+      label: __('Labels')
+    },
+    {
+      fieldname: "labels",
+      fieldtype: "Table",
+      cannot_add_rows: false,
+      in_place_edit: true,
+      data: fields.labels,
+      get_data: () => {
+        return fields.labels;
+      },
+      fields: [{
+        fieldtype: 'Int',
+        fieldname: "item_qty",
+        in_list_view: 1,
+        label: __('Item Qty')
       },
       {
-        label: __("Get data"),
-        fieldname: 'get_data',
-        fieldtype: 'Button',
-        click: () => {
-							handleDataFetch()
-						}
+        label: __("Label Qty"),
+        fieldname: 'label_qty',
+        in_list_view: 1,
+        fieldtype: 'Int'
       },
-      {
-        fieldtype: 'Column Break'
-      },
-      {
-        label: __("Reference Docname"),
-        fieldname: 'docname',
-        fieldtype: 'Dynamic Link',
-        options: 'doctype',
-      },
-      {
-        fieldtype: 'Section Break',
-        label: __('Information')
-      },
-      {
-        label: __("Item Code"),
-        fieldname: 'item_code',
-        fieldtype: 'Data'
-      },
-      {
-        label: __("Item Name"),
-        fieldname: 'item_name',
-        fieldtype: 'Data'
-      },
-      {
-        label: __("Delivery Date"),
-        fieldname: 'delivery_date',
-        fieldtype: 'Date'
-      },
-      {
-        fieldtype: 'Column Break'
-      },
-      {
-        label: __("Customer"),
-        fieldname: 'customer',
-        fieldtype: 'Data'
-      },
-      {
-        label: __("Batch"),
-        fieldname: 'batch',
-        fieldtype: 'Data'
-      },
-      {
-        fieldtype: 'Section Break',
-        label: __('Labels')
-      },
-      {
-        fieldname: "labels",
-        fieldtype: "Table",
-        cannot_add_rows: false,
-        in_place_edit: true,
-        data: fields.labels,
-        get_data: () => {
-          return fields.labels;
-        },
-        fields: [{
-            fieldtype: 'Int',
-            fieldname: "item_qty",
-            in_list_view: 1,
-            label: __('Item Qty')
-          },
-          {
-            label: __("Label Qty"),
-            fieldname: 'label_qty',
-            in_list_view: 1,
-            fieldtype: 'Int'
-          },
-        ]
-      },
+      ]
+    },
     ],
     primary_action_label: 'Print',
     primary_action(values) {
       fields = values
 
       frappe.call({
-        method: "label_printing.label_printing.api.create_labels",
+        method: "label_printing.api.print_label",
         args: {
           values: fields
         },
-        callback: function(r) {
+        callback: function (r) {
           if (r.message === 200) {
             frappe.show_alert("Label printing successful", 5);
           }
@@ -147,12 +144,11 @@ function setupLabelsDialog(page) {
     }
   });
   handleDataFetch();
-
   d.show();
-  
+
 
   // handle dpctype change
-  d.fields_dict.doctype.$input.on('change', function() {
+  d.fields_dict.doctype.$input.on('change', function () {
     d.fields_dict.docname.refresh();
   });
 
@@ -160,12 +156,24 @@ function setupLabelsDialog(page) {
     fields = d.get_values()
     if (fields.doctype && fields.docname) {
       let doc = get_doc(fields.doctype, fields.docname)
-
       if (fields.doctype === "Work Order") {
         let item = get_doc("Item", doc.production_item)
+        let se = 1
+        frappe.call({
+          method: "label_printing.api.get_associated_stockentry",
+          async: false,
+          args: { workorder: fields.docname },
+          callback(r) {
+            if (r.message) {
+              fields.batch = r.message.items[r.message.items.length - 1].batch_no
+            }
+          }
+        })
         fields.item_code = item.item_code
         fields.item_name = item.item_name
         fields.delivery_date = doc.expected_delivery_date
+        fields.labels[0].item_qty = doc.qty
+        fields.labels[0].label_qty = 1
 
         if (item.associated_company) {
           let customer = get_doc("Customer", item.associated_company)
